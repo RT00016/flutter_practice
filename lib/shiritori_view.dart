e8cd7401bb8fc34b1c56f0b48d565bb140f879fa
sssht03 <matsuo.shuta@arakawa-lab.com>
2021-01-15 20:39:13 +0900

    add text change function

--- lib/shiritori_view.dart
+++ lib/shiritori_view.dart
@@ -13,8 +13,12 @@ class _ShitritoriState extends State<Shitritori> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('name'),
-            RaisedButton(onPressed: () {}, child: Text('次へ')),
+            Text(animalsNames[index]),
+            RaisedButton(
+                onPressed: () {
+                  onButtonTap();
+                },
+                child: Text('次へ')),
             RaisedButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -24,5 +28,19 @@ class _ShitritoriState extends State<Shitritori> {
         ),
       ),
     );
+  }
+
+  List<String> animalsNames = ['ねこ', 'こぶた', 'たぬき', 'きつね'];
+  int index = 0;
+
+  void onButtonTap() {
+    setState(() {
+      if (index < 3) {
+        index++;
+      } else if (index == 3) {
+        index = 0;
+      }
+    });
+    print(index);
   }
 }
