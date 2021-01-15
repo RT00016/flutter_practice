12bf0f98a0d3c7cfb97562d209acc95f7b9eaf12
sssht03 <matsuo.shuta@arakawa-lab.com>
2021-01-15 20:14:33 +0900

    add home view

---
+++ lib/home.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'しりとり',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 50),
+          Container(
+              width: 150,
+              height: 45,
+              child: RaisedButton(
+                  onPressed: () {
+                    onButtonTap();
+                  },
+                  child: Text('スタート')))
+        ],
+      ),
+    ));
+  }
+
+  void onButtonTap() {
+    print('tap');
+  }
+}
